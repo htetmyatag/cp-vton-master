a9ccb145a5277cee7eb8ff9c3e120a5ef64f355b
htetmyatag <42917534+htetmyatag@users.noreply.github.com>
2024-10-11 21:25:10 +0700

    Update cp_dataset.py

--- cp_dataset.py
+++ cp_dataset.py
@@ -48,8 +48,7 @@ class CPDataset(data.Dataset):
         #(original code)
         #self.transform = transforms.Compose([  \
         #        transforms.ToTensor(),   \
-        #        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-        #        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]) Commented by HMA
+        #        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]) Commented by HMA
 
 
             # load data list
